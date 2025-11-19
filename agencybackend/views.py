f4b45b001e4427e8203981a70a5312fd69f31f1e
shokht-ai <shokht2007@gmail.com>
2025-11-19 08:25:07 +0500

    first comment

---
+++ agencybackend/views.py
@@ -0,0 +1,21 @@
+from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework.serializers import ModelSerializer
+from rest_framework.permissions import AllowAny
+
+
+class RegisterSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("username", "password", "email")
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
