a5537408a4f725202edb88c8f2413b9cecea8c23
shokht-ai <shokht2007@gmail.com>
2025-11-20 22:44:11 +0500

    Kichik o'zgaritish settingda

--- agencybackend/settings.py
+++ agencybackend/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT =  os.getenv('STATIC_ROOT', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
